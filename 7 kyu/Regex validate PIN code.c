214122b4275864b8d5cf9464d32db68ff971041c
Sebastien LELEU <sleleu@e3r12p17.clusters.42paris.fr>
2022-05-24 00:22:34 +0200

    kyu 7

---
+++ 7 kyu/Regex validate PIN code.c
@@ -0,0 +1,26 @@
+/*
+				DESCRIPTION
+
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+*/
+
+#include <stdbool.h>
+
+bool validate_pin(const char *pin) 
+{
+  int i;
+
+  for (i = 0; pin[i]; i++)
+    if (pin[i] < '0' || pin[i] > '9')
+          return (false);
+    if (i != 4 && i != 6)
+     return (false);
+  return (true);
+}
